d6e4178f19bde77e154deecfec6c365e0e973d2b
Rohan Prajapati <prajapatirohan638@gmail.com>
2026-09-14 11:03:43 +0530

    Update VAPID public key

--- script.js
+++ script.js
@@ -5173,7 +5173,7 @@ async function requestNotificationPermission() {
 }
 requestNotificationPermission();
 
-const VAPID_PUBLIC_KEY = "041b16aa273feebd9fd2bbe9cf0e82575fb5140658b8d1ff64a30cc6261d15775f55bc7a51494fe02ccac3e552f8ca059b232cb6b815e5e9afc9d299d732393bb7"
+const VAPID_PUBLIC_KEY = "BOPYAku-Hm8lm04RlUtj2E6HZvSdP6EY3XJVpRzpylvaqnddbJisbs72dok3aCi9jNA5TDrBO8RczX8fZ3slEsY"
 
 function urlBase64ToUint8Array(base64String) {
     const padding = "=".repeat((4 - base64String.length % 4) % 4);
